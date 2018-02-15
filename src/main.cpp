0de55af2f393fe70f3ca6c4b4311b7453371a672
sam-connolly <samwi5e@hotmail.co.uk>
2018-02-15 10:48:11 +0000

    Visual potentiometer readings

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,63 @@ C12832 lcd(D11, D13, D12, D7, D10);
 int main()
 {
     int counter = 0;
-    
-    lcd.cls();
+    AnalogIn  left(A0);
+    AnalogIn right(A1);
+
+    /**lcd.cls();
     lcd.rect(0,0,127,31,1);
     lcd.locate(2,3);
     lcd.printf("LCD basics");
-    while(true) {   // this is the third thread
+    lcd.locate(2,21);
+    lcd.printf("%d.%d.%d", MBED_MAJOR_VERSION, MBED_MINOR_VERSION,
+     MBED_PATCH_VERSION);
+    while(counter < 10)
+    {   // this is the third thread
       lcd.locate(2,12);
       lcd.printf("counter: %09d", counter);
       counter += 1;
       wait_ms(200);
     }
+
+    lcd.cls();
+    lcd.locate(0,0);
+    lcd.printf("Max number of columns: %i", lcd.columns());
+    lcd.locate(0,9);
+    lcd.printf("Max number of rows: %i", lcd.rows());
+    wait(2);
+
+    while(1)
+    {
+      lcd.cls();
+      lcd.locate(0,0);
+      lcd.printf("Left dial %f \n",  left.read() );
+      lcd.locate(0,9);
+      lcd.printf("Right dial %f \n", right.read() );
+      wait( 1.0 );
+    }
+
+    lcd.cls();
+    lcd.line(0, 0, lcd.width(), lcd.height(), 1);
+    wait_ms(500);
+    lcd.line(0, lcd.height(), lcd.width(), 0, 1);
+    wait_ms(500);
+    lcd.rect(0, 0, lcd.width(), lcd.height(), 1);
+    wait(2);**/
+
+    while(true)
+    {
+      lcd.cls();
+      lcd.locate(0, 0);
+      lcd.printf("L: ");
+      lcd.rect(10, 0, 110, 5, 1);
+      int lWidth = (left.read()*100) + 10;
+      lcd.fillrect(10, 0, lWidth, 5, 1);
+
+      lcd.locate(0, 9);
+      lcd.printf("R: ");
+      lcd.rect(10, 9, 110, 14, 1);
+      int rWidth = (right.read()*100) + 10;
+      lcd.fillrect(10, 9, rWidth, 14, 1);
+    }
+
 }
